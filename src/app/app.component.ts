81523075dd79c775fe0124dabdba72cbb73053ca
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-11-18 20:16:41 -0500

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-first-project-sunat';
+
+  constructor() {
+    console.log('Creando El Componente');
+  }
+  ngOnInit(): void {
+    console.log('El Componente esta iniciando');
+  }
 }

1b93b08fe055f571e1885f1cb323ce1e8ca31beb
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-11-22 20:12:00 -0500

    rendering HTTP and Suscribing

--- src/app/route/photo-album-http/photo-album-http.component.ts
+++ src/app/route/photo-album-http/photo-album-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Photo } from './model/photo.model';
 import { PhotoAlbumService } from './services/photo-album.service';
 
 @Component({
@@ -7,11 +8,23 @@ import { PhotoAlbumService } from './services/photo-album.service';
   styleUrls: ['./photo-album-http.component.scss']
 })
 export class PhotoAlbumHttpComponent implements OnInit {
+  photos: Photo[] = [];
 
   constructor(private photoAlbumService: PhotoAlbumService) { }
 
   ngOnInit(): void {
-    this.photoAlbumService.getAllPhotos().subscribe(console.log);
+    this.photoAlbumService.getAllPhotos().subscribe(photos => {
+      this.photos = photos.slice(10,20);
+    }, 
+    error => console.error(error),
+    () => console.log('Termine de traer las fotos!!'));
+
+    this.photoAlbumService.getAllPhotos().subscribe({
+      next: console.log,
+      error: console.error,
+      complete: () => console.log('Termine otra vez')
+    });
+
   }
 
 }
